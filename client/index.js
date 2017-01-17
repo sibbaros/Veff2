902be6540280070c115e95c9e8c206a9abebbbe4
alivarsdotttir <alexandralif@hotmail.com>
2017-01-17 13:31:20 +0000

    connection is up

---
+++ client/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    console.log("Ready!");
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.moveTo(0, 0);
+    ctx.lineTo(200, 100);
+    ctx.stroke();
+});
