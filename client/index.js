75bb37a1934ddbefac92235c1f13c068cba46ff3
eydisar <eydisar@gmail.com>
2017-01-17 21:59:48 +0000

    rosa fínn ferningur ví

--- client/index.js
+++ client/index.js
@@ -1,8 +1,41 @@
 $(document).ready(function() {
-    console.log("Ready!");
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.moveTo(0, 0);
-    ctx.lineTo(200, 100);
-    ctx.stroke();
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+    var drawing = false;
+    var x;
+    var y;
+
+    $("#myCanvas").mousedown(function(e) {
+
+        x = e.pageX - this.offsetLeft;
+        y = e.pageY - this.offsetTop;
+
+        drawing = true;
+    });
+
+    $("#myCanvas").mousemove(function(e) {
+        if (drawing === true) {
+            var endX = e.pageX - x;
+            var endY = e.pageY - y;
+
+            context.clearRect(0, 0, 600, 200);
+            context.beginPath();
+            context.moveTo(x, y);
+            context.strokeRect(x, y, endX, endY);
+            context.stroke();
+        }
+
+    });
+
+    $("#myCanvas").mouseup(function(e) {
+
+        drawing = false;
+
+    });
+
 });
+
+//git add .
+//git commit -m ""
+//git pull 
+//git push
