dbc9f9f01414216299dda24023b825e0da9ba37a
alivarsdotttir <alexandralif@hotmail.com>
2017-01-18 16:06:47 +0000

    now we can draw many rectangles

--- client/index.js
+++ client/index.js
@@ -1,39 +1,61 @@
+var drawnShapes = [];
+var currShape = undefined;
+var startX;
+var startY;
+var isDrawing = false;
+
 $(document).ready(function() {
 
     console.log("Ready!");
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    //ctx.moveTo(0, 0);
-    //ctx.lineTo(200, 100);
-    //ctx.stroke();
-    drag = false;
-    var startX;
-    var startY;
-    var isDrawing = false;
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    function rectangle(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    rectangle.prototype.draw = function() {
+        ctx.strokeStyle = "black";
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+    }
 
     $("#myCanvas").mousedown(function(e) {
+        console.log("clicked");
         var x = e.pageX - this.offsetLeft;
         var y = e.pageY - this.offsetTop;
 
         startX = x;
         startY = y;
         isDrawing = true;
+        currShape = new rectangle(x, y);
     });
 
     $("#myCanvas").mousemove(function(e) {
         if (isDrawing === true) {
-            var x = e.pageX - startX;
-            var y = e.pageY - startY;
-
-            ctx.clearRect(0, 0, 600, 200);
-            ctx.strokeRect(startX, startY, x, y);
+            var w = e.pageX - startX;
+            var h = e.pageY - startY;
+            currShape.width = w;
+            currShape.height = h;
+            redraw();
+            currShape.draw();
         }
-
     });
 
     $("#myCanvas").mouseup(function(e) {
         isDrawing = false;
+        drawnShapes.push(currShape);
+        redraw();
     })
+
+    function redraw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (var i = 0; i < drawnShapes.length; i++) {
+            drawnShapes[i].draw();
+        }
+    }
 
 });
 
